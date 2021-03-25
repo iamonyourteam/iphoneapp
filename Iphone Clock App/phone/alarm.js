675765ace1a86702d29d626c3d3b61dc53aeeab4
iamonyourteam <73721770+iamonyourteam@users.noreply.github.com>
2021-03-24 22:00:38 -0400

    alarm numpad
    added alarm numpad functionality. Opens and closes with Add Alarm Window, Numpad buttons change value of time.

--- Iphone Clock App/phone/alarm.js
+++ Iphone Clock App/phone/alarm.js
@@ -1,17 +1,21 @@
-let newTime = 0
-let newRepeat = "Never"
-let newLabel = "Alarm"
-let newSound = "Radar"
-let newSnooze = True
+var numpadentry = []
+var alarmConfigTemp = []
+var alarmConfig =
+    [{ alarmNumTime: "19:00", alarmAmPm: "PM", alarmTimerbottom: "Timer1" },
+    { alarmNumTime: "18:00", alarmAmPm: "PM", alarmTimerbottom: "Timer1" },
+    { alarmNumTime: "8:40", alarmAmPm: "AM", alarmTimerbottom: "Bedtime" },
+    { alarmNumTime: "8:30", alarmAmPm: "AM", alarmTimerbottom: "C" }]
 
 
 function alarmAddPage() {
     document.querySelector("#selAlarmPage").style.top = '5vh'
+    document.querySelector("#numpad").style.top = '50vh'
     console.log("Hello ")
 }
 
 function alarmPageCancel() {
     document.querySelector("#selAlarmPage").style.top = '100vh'
+    document.querySelector("#numpad").style.top = '100vh'
 }
 function alarmPageSave() {
     // add content of add alarm page to array of alarms 
@@ -34,12 +38,88 @@ function alarmToggle(n) {
     }
 }
 
-let alarmconfig = [{ alarmNumTime: "19:00", alarmAmPm: "PM", alarmTimerbottom: "Timer1" }, { alarmNumTime: "18:00", alarmAmPm: "PM", alarmTimerbottom: "Timer1" }, { alarmNumTime: "8:40", alarmAmPm: "AM", alarmTimerbottom: "Bedtime" }, { alarmNumTime: "8:30", alarmAmPm: "AM", alarmTimerbottom: "C" }]
-alarmconfig = alarmconfig.sort((a, b) => (parseInt(a.alarmNumTime.split(":").join("")) > parseInt(b.alarmNumTime.split(":").join(""))) ? 1 : -1)
-console.table(alarmconfig)
 
-for (let i = 0; i < alarmconfig.length; i++) {
+alarmConfig = alarmConfig.sort((a, b) => (parseInt(a.alarmNumTime.split(":").join("")) > parseInt(b.alarmNumTime.split(":").join(""))) ? 1 : -1)
+console.table(alarmConfig)
+
+for (let i = 0; i < alarmConfig.length; i++) {
     document.querySelector(`#alarm${i}`).childNodes[1].childNodes[1].childNodes[1].innerText = alarmconfig[i].alarmNumTime
     document.querySelector(`#alarm${i}`).childNodes[1].childNodes[1].childNodes[3].innerText = alarmconfig[i].alarmAmPm
     console.log(document.querySelector(`#alarm${i}`).childNodes[3].innerText = alarmconfig[i].alarmTimerbottom)
 }
+
+
+function AMPMToggel(num) {
+    if (num === 1) {
+        if (document.querySelector("#AM").className !== "AMPMON") {
+
+            document.querySelector("#AM").className = "AMPMON"
+            document.querySelector("#PM").className = "AMPMOFF"
+        }
+    }
+    else {
+        if (document.querySelector("#PM").className !== "AMPMON") {
+            document.querySelector("#PM").className = "AMPMON"
+            document.querySelector("#AM").className = "AMPMOFF"
+        }
+    }
+}
+
+
+function alarmPageSave() {
+    let tempTime = { alarmNumTime: (document.querySelector("#timeSelectNum").innerText) }
+    let tempAMPM = { alarmAmPm: (document.querySelector(".AMPMON").innerText) }
+    let tempRepeat = { alarmTimerRepeat: (document.querySelectorAll(".optionValue")[0].innerText) }
+    let tempLabel = { alarmTimerbottom: (document.querySelectorAll(".optionValue")[1].innerText) }
+    let tempSound = { alarmNumSound: (document.querySelectorAll(".optionValue")[2].innerText) }
+
+    alarmConfigTemp.push(tempTime, tempAMPM, tempRepeat, tempLabel, tempSound)
+    alarmConfig.push(alarmConfigTemp)
+    console.log(alarmConfig)
+
+}
+
+
+function updatenumpress() {
+    if (numpadentry.length === 4) {
+        document.querySelector("#timeSelectHr").innerText = `${numpadentry[0]}${numpadentry[1]}`
+        document.querySelector("#timeSelectMin").innerText = `${numpadentry[2]}${numpadentry[3]}`
+    } else if (numpadentry.length === 3) {
+        document.querySelector("#timeSelectHr").innerText = `0${numpadentry[0]}`
+        document.querySelector("#timeSelectMin").innerText = `${numpadentry[1]}${numpadentry[2]}`
+    } else if (numpadentry.length === 2) {
+        document.querySelector("#timeSelectHr").innerText = "00"
+        document.querySelector("#timeSelectMin").innerText = `${numpadentry[0]}${numpadentry[1]}`
+    } else if (numpadentry.length === 1) {
+        document.querySelector("#timeSelectHr").innerText = "00"
+        document.querySelector("#timeSelectMin").innerText = `0${numpadentry[0]}`
+    } else if (numpadentry.length === 0) {
+        document.querySelector("#timeSelectHr").innerText = "00"
+        document.querySelector("#timeSelectMin").innerText = `00`
+    }
+}
+
+function numpress(a) {
+
+    console.log(a)
+    numpadentry.push(a)
+    console.log(numpadentry)
+    while (numpadentry.join("") > 1259) {
+        numpadentry.shift()
+
+    }
+
+    console.log(numpadentry)
+
+    updatenumpress()
+
+}
+
+function erase() {
+    numpadentry.pop()
+    console.log(numpadentry)
+    updatenumpress()
+}
+
+
+document.querySelector("#AM").className = "AMPMON"
